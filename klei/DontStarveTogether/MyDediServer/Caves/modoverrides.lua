23650f93b99a9dddf001447538f9703951bc9a66
Madeleine Neumann <madeleine.neumann@9elements.com>
2019-12-02 12:30:43 +0100

    adds tutorial

---
+++ klei/DontStarveTogether/MyDediServer/Caves/modoverrides.lua
@@ -0,0 +1,28 @@
+return {
+  ["workshop-356930882"]={ configuration_options={ uses=100, uses2=100 }, enabled=true },
+  ["workshop-362175979"]={ configuration_options={ ["Draw over FoW"]="disabled" }, enabled=true },
+  ["workshop-378160973"]={
+    configuration_options={
+      ENABLEPINGS=true,
+      FIREOPTIONS=1,
+      OVERRIDEMODE=false,
+      SHAREMINIMAPPROGRESS=true,
+      SHOWFIREICONS=true,
+      SHOWPLAYERICONS=true,
+      SHOWPLAYERSOPTIONS=3 
+    },
+    enabled=true 
+  },
+  ["workshop-466732225"]={ configuration_options={  }, enabled=true },
+  ["workshop-623749604"]={
+    configuration_options={
+      Craft="Easy",
+      Destroyable="DestroyByAll",
+      FoodSpoilage=1,
+      Language="En",
+      Position="Center",
+      Slots=80 
+    },
+    enabled=true 
+  } 
+}
